307dffbcdf340ebb8802b89ad31d6eec24fa76fb
Julio González Martín <jagonzalez@getinsoft.com>
2022-08-01 17:05:45 -0500

    Improve css, separate logic in different components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import "./App.css";
 import "./components/ExpenseItem";
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: 'Monitor Game Factor 27" QHD',
+      amount: 4999.5,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "Mouse G Pro",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e3",
+      title: "Teclado Logitec G-350",
+      amount: 2455.5,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "Escritorio en L (madera)",
+      amount: 3400,
+      date: new Date(2022, 5, 12),
+    },
+  ];
+
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <div>
       <h2>Gastos realizados</h2>
-      <ExpenseItem />
+      <Expenses items={expenses} />
     </div>
   );
 }
