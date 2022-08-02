978b04a9e26e880f29cc54d5a41f33e098a24f0e
Julio González Martín <jagonzalez@getinsoft.com>
2022-08-02 16:35:13 -0500

    Create card component

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import "./Card.css";
+
+function Card(props) {
+  //props.className contains the classes from the children
+  const classes = "card " + props.className;
+  return (
+    //props.children is the content of the card (between the open and close tag).
+    <div className={classes}>{props.children}</div> 
+  );
+}
+
+export default Card;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,19 @@
 import "./ExpenseItem.css";
+import "./ExpenseDate";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 7, 1);
-  const expenseTitle = 'Monitor Game factor 27" QHD';
-  const expenseAmount = 4899.50;
+function ExpenseItem(props) {
 
   return (
     <>
-      <div className="expense-item">
-        <div>{expenseDate.toISOString()}</div>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
         <div className="expense-item__description">
-          <h2>{expenseTitle}</h2>
-          <div className="expense-item__price">${expenseAmount}</div>
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
         </div>
-      </div>
+      </Card>
     </>
   );
 }
