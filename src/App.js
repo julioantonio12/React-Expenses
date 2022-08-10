c56e06b89f8527acbc68583207bb6ac1efcc5af5
Julio González Martín <jagonzalez@getinsoft.com>
2022-08-10 17:13:05 -0500

    Output dynamic list

--- src/App.js
+++ src/App.js
@@ -2,38 +2,42 @@ import "./App.css";
 import "./components/Expenses/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: 'Monitor Game Factor 27" QHD',
+    amount: 4999.5,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "Mouse G Pro",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: "e3",
+    title: "Teclado Logitec G-350",
+    amount: 2455.5,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "Escritorio en L (madera)",
+    amount: 3400,
+    date: new Date(2022, 5, 12),
+  },
+];
 
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: 'Monitor Game Factor 27" QHD',
-      amount: 4999.5,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: "e2",
-      title: "Mouse G Pro",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-    },
-    {
-      id: "e3",
-      title: "Teclado Logitec G-350",
-      amount: 2455.5,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "Escritorio en L (madera)",
-      amount: 3400,
-      date: new Date(2022, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-  const addExpenseHandler = expense => {
-    console.log("In App.js");
-    console.log(expense);
+  const addExpenseHandler = (expense) => {
+    setExpenses((previousExpenses) => {
+      return [expense, ...previousExpenses];
+    });
   }
 
   return (
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,39 +1,35 @@
-import ExpenseItem from "./ExpenseItem";
-import './Expenses.css';
-import Card from "../UI/Card";
-import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
-  
+
   const changeFilterHandler = (yearValue) => {
     setFilteredYear(yearValue);
     console.log(yearValue);
-  }
+  };
 
   return (
     <>
-    <Card className="expenses">
-      <ExpensesFilter onChangeFilter={changeFilterHandler} selected={filteredYear}/>
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-    </Card>
+      <Card className="expenses">
+        <ExpensesFilter
+          onChangeFilter={changeFilterHandler}
+          selected={filteredYear}
+        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
     </>
   );
-}
+};
 
 export default Expenses;
